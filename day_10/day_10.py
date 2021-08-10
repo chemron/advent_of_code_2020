3eef08d4f4b7b802b20c28b5cf29398c9e0fa8d8
chemron <camerontasmith@gmail.com>
2021-08-10 19:21:31 +1000

    day 10

--- day_10/day_10.py
+++ day_10/day_10.py
@@ -1,7 +1,8 @@
+# https://adventofcode.com/2020/day/10
 import numpy as np
 
 
-f = open('test_input.txt', 'r')
+f = open('input.txt', 'r')
 adapters = f.read().split()
 adapters = list(map(int, adapters))
 adapters.sort()
@@ -12,19 +13,28 @@ end = adapters[-1] + 3
 differences = np.array(adapters + [end]) - \
               np.array([start] + adapters)
 
-print(differences)
 # no. one jolt differences
-n_one_j = 0
+n_one_j = len(np.where(differences==1)[0])
 
 # no. three jolt differences
-n_three_j = 0
-
-for i, i_val in enumerate(adapters[:-1]):
-    j = i + 1
-    j_val = adapters[j]
-    if j_val - i_val == 1:
-        n_one_j += 1
-    elif j_val - i_val == 3:
-        n_three_j += 1
+n_three_j = len(np.where(differences==3)[0])
 
 print(f'# 1-jolt diff x # 3-jolt diff = {n_one_j*n_three_j} ')
+
+# part 2
+
+# num_ways[i] = number of ways to connect to adapter with voltage i
+num_ways = np.zeros(adapters[-1] + 1, dtype=np.int64)
+
+# one way to connect to the zeroth adapter (the outlet)
+num_ways[0] = 1
+
+for i in range(1, adapters[-1] + 1):
+    if i in adapters:
+        left = max(0, i-3)
+        n = np.sum(num_ways[left:i])
+        num_ways[i] = n
+    else:
+        num_ways[i] = 0
+
+print(f"Number of arrangements: {num_ways[-1]}")
